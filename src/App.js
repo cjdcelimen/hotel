a4d8b9c81f3170dc92e89e77fe9e5269f176517a
CJ Celimen <cjdcelimen@gmail.com>
2020-11-13 21:16:07 +0800

    Initial navbar done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Home></Home>
-      <Rooms></Rooms>
-      <SingleRoom></SingleRoom>
-      <Error></Error>
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/rooms' component={Rooms} />
+        <Route exact path='/rooms/:singleroom' component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </>
   );
 }
