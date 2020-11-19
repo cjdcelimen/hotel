a52fb45b18bca49e41f65f250a1c9b528d2075b4
CJ Celimen <cjdcelimen@gmail.com>
2020-11-19 20:32:21 +0800

    Update room list

--- src/context.js
+++ src/context.js
@@ -9,7 +9,7 @@ class RoomProvider extends Component {
         rooms: [],
         sortedRooms: [],
         featuredRooms: [],
-        loading: true
+        loading: false
     }
 
     componentDidMount() {
@@ -57,3 +57,13 @@ class RoomProvider extends Component {
 const RoomConsumer = RoomContext.Consumer;
 
 export { RoomProvider, RoomConsumer, RoomContext }
+
+export function withRoomConsumer(Component) {
+  return function ConsumerWrapper(props) {
+    return (
+      <RoomConsumer>
+        {value => <Component {...props} context={value} />}
+      </RoomConsumer>
+    );
+  };
+}
